96ec89dbe5c7095a97d7085a0eb0e26a1433d6c9
spollard1810 <spollard1810@gmail.com>
2025-04-21 12:22:15 -0500

    forget to add as part of prev commit; added test logic

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 from connect import NetworkDevice
 
 device = NetworkDevice("cisco_ios", "192.168.1.1", "admin", "password")
-
+print(device.get_facts())
 print(device.get_cdp_neighbors())
-print(device.get_device_type())
 device.disconnect()
